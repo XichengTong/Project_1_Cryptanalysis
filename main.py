b327106318bc76f8b43e940ed47c43a0244bfe43
Xicheng Tong <137130203+XichengTong@users.noreply.github.com>
2024-09-22 13:37:43 -0400

    front end adding

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    # Reading from stdin
+    input_text = sys.stdin.read()
+    
+    # Printing the input text
+    print("You entered:", input_text)
+
+if __name__ == "__main__":
+    main()
